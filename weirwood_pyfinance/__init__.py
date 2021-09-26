c0f4c81904d924d970f358b1a38b6ca140353d1a
Alex Faraday <encalado.masia@gmail.com>
2021-09-26 14:27:07 +0200

    Initial commit

---
+++ weirwood_pyfinance/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Weirwood PyFinance."""
+
+__author__ = "Alex Encalado Masia"
+__email__ = "encalado.masia@gmail.com"
+# Do not edit this string manually, always use bumpversion
+# Details in CONTRIBUTING.md
+__version__ = "0.0.0"
+
+
+def get_module_version():
+    return __version__
+
+
+from .example import Example  # noqa: F401
